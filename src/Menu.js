ce4b4266fead33f620a5b1da81fbc8e820218116
sakshamarora1 <sakshamarora1001@gmail.com>
2021-05-25 17:07:23 +0530

    Add button to download the font

--- src/Menu.js
+++ src/Menu.js
@@ -1,34 +1,57 @@
 import { Button } from '@material-ui/core';
 import { useState } from 'react';
+import Grid from '@material-ui/core/Grid'
 import React from 'react';
+
 const Menu = () => {
     const [name, setName] = useState("");
     const [image, setImage] = useState("");
+    const [font, setFont] = useState("");
+    const [fetching, setFetching] = useState(false);
 
     function sendImage(event) {
         event.preventDefault();
         let formData = new FormData();
         formData.append("image", event.target.inputfile.files[0]);
+        var stat = -1;
+        var path;
+        var font_url;
+        setFetching(true);
         fetch(
-            "http://localhost:5000/handwrite/input",
+            "http://handwritetest.herokuapp.com/handwrite/input",
             {
                 method: 'POST',
                 body: formData
             }
         ).then((r) => r.json()).then(async (data) => {
-            const path = data.path;
-            var stat = -1;
+            path = data.path;
             for (let i = 0; i < 10; i++) {
-                fetch("http://localhost:5000/handwrite/status/" + path).then((r) => r.json()).then((status) => {
+                fetch("http://handwritetest.herokuapp.com/handwrite/status/" + path).then((r) => r.json()).then((status) => {
                     if (status.status === 0) {
                         console.log("Font file ready!");
                         stat = 0;
                     }
                 })
-                if (stat == 0) {
+                console.log(stat);
+                if (stat === 0) {
                     break;
                 }
                 await new Promise(r => setTimeout(r, 5000));
+            }
+        }).then(() => { 
+            if (stat === 0) {
+                fetch(
+                    "http://handwritetest.herokuapp.com/handwrite/fetch/" + path,
+                    {
+                        method: 'POST'
+                    }
+                ).then((r) => r.blob()).then(data => {
+                    console.log(data);
+                    font_url = URL.createObjectURL(data);
+                    console.log(font_url);
+                    setFont(font_url);
+                    setFetching(false);
+                });
             }
         });
     }
@@ -36,17 +59,27 @@ const Menu = () => {
     return (
         <div className="menu">
             <form onSubmit={(e) => sendImage(e)}>
-                <div className="input-image">
-                    <img src={image} />
-
-                </div>
-                <label id="image">Upload Image: </label>
-                <input name="inputfile" id="contained-button-file" type="file" value={name} onChange={(e) => {
-                    setName(e.target.value);
-                    setImage(URL.createObjectURL(e.target.files[0]));
-                }} accept="image/*" ></input>
-                <Button type="submit" variant="outlined">Create Font!</Button>
+                <Grid container direction="row" justify="space-around" alignItems="center">
+                    <div className="input-image">
+                        <img src={image} />
+                    </div>
+                    <div>
+                        <label id="image">Upload Image: </label>
+                        <input name="inputfile" id="contained-button-file" type="file" value={name} onChange={(e) => {
+                            setName(e.target.value);
+                            setImage(URL.createObjectURL(e.target.files[0]));
+                        }} accept="image/*" ></input>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <Button type="submit" variant="outlined" disabled={fetching}>Create Font!</Button>
+                        &emsp;&emsp;&emsp;
+                        <Button variant="outlined" href={font} download="font.ttf" style={{display: Boolean(font) ? "" : "none"}}>Download your font</Button>
+                    </div>
+                </Grid>
             </form>
+            <br/>
+            <br/>
         </div>
     );
 }
